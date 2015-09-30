942abb811df56f13620f7e3e8155740bff949bf4
chenhaoyang <chenhaoyang0529@gmail.com>
2015-09-30 18:46:22 +0800

    beginning version

---
+++ demo/demo.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    var setting = {
+        //animationSpeed: 'slow'
+    };
+
+    var $number = $("#number-wrapper");
+    var i = 1;
+    var diff = 29;
+    (function test() {
+        var currentNumber = i + diff;
+        //when you init it, you should set originNumber equal currentNumber
+        var originNumber = i==1? currentNumber : i;
+        $number.myNumberScroll(setting, originNumber, currentNumber);
+        i = i + diff;
+        if(i < 1000) {
+            setTimeout(test, 1000);
+        }
+    })();
+
+});
